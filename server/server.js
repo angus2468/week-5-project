3e9feaee6bd07d567ef01cbade19c44b3826cfe6
LiamCollins <avengedsevenfold.lc@gmail.com>
2025-02-03 14:04:47 +0000

    pseudo code added

--- server/server.js
+++ server/server.js
@@ -7,3 +7,19 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 dotenv.config()
+
+//pseudo code.
+
+//using databases from supabase, allow program to access for movie API(maybe) and book API(maybe) and use a DB to hold user info
+
+//DB connection information
+
+//get requests for fetching current data
+
+//post requests for adding information to watched/read DB
+
+//setting reminders using general date and time stamps
+
+//delete reminders
+
+//update requests

451896328398862918202af02484cafec6267fd1
Angus <angus.baker2468@gmail.com>
2025-02-03 15:01:32 +0000

    server set up for fetching userInfo and updated seed.sql

--- server/server.js
+++ server/server.js
@@ -1,15 +1,22 @@
-import express from 'express'
-import cors from 'cors'
-import pg from 'pg'
-import dotenv from 'dotenv'
+import express, { response } from "express";
+import cors from "cors";
+import pg from "pg";
+import dotenv from "dotenv";
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-dotenv.config()
+const app = express();
+app.use(cors());
+app.use(express.json());
+dotenv.config();
 
-//pseudo code.
+const db = new pg.Pool({
+  connectionString: process.env.DB_CONN,
+});
 
+app.get("/", (req, res) => response.json("Hello there (◕ᴥ◕ʋ)"));
+
+app.get("/userInfo", async (req, res) => {
+  const result = await db.query("SELECT * FROM userInfo");
+});
 //using databases from supabase, allow program to access for movie API(maybe) and book API(maybe) and use a DB to hold user info
 
 //DB connection information
@@ -23,3 +30,6 @@ dotenv.config()
 //delete reminders
 
 //update requests
+app.listen(8080, () => {
+  console.log("Ther server is running and lsitening on port 8080");
+});

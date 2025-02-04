2202b5c3728f6e8669db92f21c6fc1643ad612fe
Aishah Perks <byaishah.altaf@gmail.com>
2025-02-04 13:34:59 +0000

    fixed the add reminder button

--- client/src/main.js
+++ client/src/main.js
@@ -5,26 +5,20 @@
 //UI updates
 
 //post to server updates based on reminders, watch history etc
-const createNav = document.getElementById("createNav");
-const createButton = document.getElementById("createButton");
-
 
 createButton.addEventListener("click", handleCreate);
 
 const createNav = document.getElementById("createNav");
 const createButton = document.getElementById("createButton");
 
-
-const addChecklist = document.getElementById('addChecklist')
-const addBooks = document.getElementById('addBooks')
-const addMovies = document.getElementById('addMovies')
-const addReminders = document.getElementById('addReminders')
-
-createButton.addEventListener('click', handleCreate)
-
+const addChecklist = document.getElementById("addChecklist");
+const addBooks = document.getElementById("addBooks");
+const addMovies = document.getElementById("addMovies");
+const addReminders = document.getElementById("addReminders");
 
 createButton.addEventListener("click", handleCreate);
 
+createButton.addEventListener("click", handleCreate);
 
 function handleCreate() {
   if (createNav.hasAttribute("hidden")) {
@@ -34,143 +28,138 @@ function handleCreate() {
   }
 }
 
-}
-
-
-addChecklist.addEventListener('click', checklistForm)
-
+addChecklist.addEventListener("click", checklistForm);
 
 function checklistForm() {
-  const formDiv = document.createElement('div')
-  const createForm = document.createElement('form')
-  const taskLabel = document.createElement('label')
-  const taskInput = document.createElement('input')
-  const submitButton = document.createElement('button')
+  const formDiv = document.createElement("div");
+  const createForm = document.createElement("form");
+  const taskLabel = document.createElement("label");
+  const taskInput = document.createElement("input");
+  const submitButton = document.createElement("button");
 
-  createForm.setAttribute('class', 'checklistForm')
-  taskLabel.setAttribute('for', 'task')
-  taskInput.setAttribute('name', 'task')
-  taskInput.setAttribute('type', 'text')
-  submitButton.setAttribute('type', 'submit')
+  createForm.setAttribute("class", "checklistForm");
+  taskLabel.setAttribute("for", "task");
+  taskInput.setAttribute("name", "task");
+  taskInput.setAttribute("type", "text");
+  submitButton.setAttribute("type", "submit");
 
-  formDiv.appendChild(createForm)
-  formDiv.appendChild(taskLabel)
-  formDiv.appendChild(taskInput)
+  formDiv.appendChild(createForm);
+  formDiv.appendChild(taskLabel);
+  formDiv.appendChild(taskInput);
 
-  taskLabel.innerText = 'Item:'
-  submitButton.innerText = 'Add'
+  taskLabel.innerText = "Item:";
+  submitButton.innerText = "Add";
 
-  createNav.appendChild(formDiv)
-  
-  createForm.addEventListener('submit', (event) => {
-    handleSubmit(event)
-  })
+  createNav.appendChild(formDiv);
+
+  createForm.addEventListener("submit", (event) => {
+    handleSubmit(event);
+  });
 
   function handleSubmit(event) {
-    event.preventDefault()
-    createNav.setAttribute('hidden', '')
+    event.preventDefault();
+    createNav.setAttribute("hidden", "");
   }
-  
 }
 
-addBooks.addEventListener('click', bookForm)
+addBooks.addEventListener("click", bookForm);
 function bookForm() {
-const bookDiv = document.createElement("div")
-const bookForm = document.createElement("form");
-const bookName = document.createElement("label");
-const bookNameData = document.createElement("input");
-const bookGenre = document.createElement("label");
-const bookGenreData = document.createElement("input");
-const bookAuthor = document.createElement("label");
-const bookAuthorData = document.createElement("input");
-const bookBtn = document.createElement("button");
+  const bookDiv = document.createElement("div");
+  const bookForm = document.createElement("form");
+  const bookName = document.createElement("label");
+  const bookNameData = document.createElement("input");
+  const bookGenre = document.createElement("label");
+  const bookGenreData = document.createElement("input");
+  const bookAuthor = document.createElement("label");
+  const bookAuthorData = document.createElement("input");
+  const bookBtn = document.createElement("button");
 
-bookName.setAttribute("for", "bookName");
-bookNameData.setAttribute("name", "bookName");
-bookGenre.setAttribute("for", "bookGenre");
-bookGenreData.setAttribute("name", "bookGenre");
-bookAuthor.setAttribute("for", "bookAuthor");
-bookAuthorData.setAttribute("name", "bookAuthor");
-bookNameData.setAttribute("type", "text");
-bookGenreData.setAttribute("type", "text");
-bookAuthorData.setAttribute("type", "text");
-bookBtn.setAttribute("type", "submit")
+  bookName.setAttribute("for", "bookName");
+  bookNameData.setAttribute("name", "bookName");
+  bookGenre.setAttribute("for", "bookGenre");
+  bookGenreData.setAttribute("name", "bookGenre");
+  bookAuthor.setAttribute("for", "bookAuthor");
+  bookAuthorData.setAttribute("name", "bookAuthor");
+  bookNameData.setAttribute("type", "text");
+  bookGenreData.setAttribute("type", "text");
+  bookAuthorData.setAttribute("type", "text");
+  bookBtn.setAttribute("type", "submit");
 
-bookName.innerText = "Title:";
-bookGenre.innerText = "Genre:";
-bookAuthor.innerText = "Author:";
-bookBtn.innerText = "Add";
+  bookName.innerText = "Title:";
+  bookGenre.innerText = "Genre:";
+  bookAuthor.innerText = "Author:";
+  bookBtn.innerText = "Add";
 
-bookForm.appendChild(bookName)
-bookForm.appendChild(bookNameData);
-bookForm.appendChild(bookGenreData);
-bookForm.appendChild(bookGenreData);
-bookForm.appendChild(bookAuthor);
-bookForm.appendChild(bookAuthorData);
-bookForm.appendChild(bookBtn);
-bookDiv.appendChild(bookForm);
-createNav.appendChild(bookDiv);
+  bookForm.appendChild(bookName);
+  bookForm.appendChild(bookNameData);
+  bookForm.appendChild(bookGenreData);
+  bookForm.appendChild(bookGenreData);
+  bookForm.appendChild(bookAuthor);
+  bookForm.appendChild(bookAuthorData);
+  bookForm.appendChild(bookBtn);
+  bookDiv.appendChild(bookForm);
+  createNav.appendChild(bookDiv);
 
-bookForm.addEventListener('submit', (event) => {
-    handleSubmit(event)
-  })
+  bookForm.addEventListener("submit", (event) => {
+    handleSubmit(event);
+  });
 
   function handleSubmit(event) {
-    event.preventDefault()
-    createNav.setAttribute('hidden', '')
+    event.preventDefault();
+    createNav.setAttribute("hidden", "");
   }
 }
 
-addMovies.addEventListener('click', moviesForm)
+addMovies.addEventListener("click", moviesForm);
 
 function moviesForm() {
-  const formDiv = document.createElement('div')
-  const createForm = document.createElement('form')
-  
-  const nameLabel = document.createElement('label')
-  const genreLabel = document.createElement('label')
-  const languageLabel = document.createElement('label')
+  const formDiv = document.createElement("div");
+  const createForm = document.createElement("form");
 
-  const nameInput = document.createElement('input')
-  const genreInput = document.createElement('input')
-  const languageInput = document.createElement('input')
+  const nameLabel = document.createElement("label");
+  const genreLabel = document.createElement("label");
+  const languageLabel = document.createElement("label");
 
-  const submitButton = document.createElement('button')
+  const nameInput = document.createElement("input");
+  const genreInput = document.createElement("input");
+  const languageInput = document.createElement("input");
 
-  nameInput.setAttribute('name', 'name')
-  genreInput.setAttribute('name', 'genre')
-  languageInput.setAttribute('name', 'language')
-  nameLabel.setAttribute('for', 'name')
-  genreLabel.setAttribute('for', 'genre')
-  languageLabel.setAttribute('for', 'language')
+  const submitButton = document.createElement("button");
 
-  nameInput.setAttribute('type', 'text')
-  genreInput.setAttribute('type', 'text')
-  languageInput.setAttribute('type', 'text')
-  submitButton.setAttribute('type', 'submit')
-  
-  nameLabel.innerText = 'Title:'
-  genreLabel.innerText = 'Genre:'
-  languageLabel.innerText = 'Language:'
-  submitButton.innerText = 'Add'
+  nameInput.setAttribute("name", "name");
+  genreInput.setAttribute("name", "genre");
+  languageInput.setAttribute("name", "language");
+  nameLabel.setAttribute("for", "name");
+  genreLabel.setAttribute("for", "genre");
+  languageLabel.setAttribute("for", "language");
 
-  formDiv.appendChild(nameLabel)
-  formDiv.appendChild(nameInput)
-  formDiv.appendChild(genreLabel)
-  formDiv.appendChild(genreInput)
-  formDiv.appendChild(languageLabel)
-  formDiv.appendChild(languageInput)
-  formDiv.appendChild(submitButton)
+  nameInput.setAttribute("type", "text");
+  genreInput.setAttribute("type", "text");
+  languageInput.setAttribute("type", "text");
+  submitButton.setAttribute("type", "submit");
 
-  createNav.appendChild(formDiv)
-  
-  createForm.addEventListener('submit', (event) => {
-    handleSubmit(event)
-  })
+  nameLabel.innerText = "Title:";
+  genreLabel.innerText = "Genre:";
+  languageLabel.innerText = "Language:";
+  submitButton.innerText = "Add";
+
+  formDiv.appendChild(nameLabel);
+  formDiv.appendChild(nameInput);
+  formDiv.appendChild(genreLabel);
+  formDiv.appendChild(genreInput);
+  formDiv.appendChild(languageLabel);
+  formDiv.appendChild(languageInput);
+  formDiv.appendChild(submitButton);
+
+  createNav.appendChild(formDiv);
+
+  createForm.addEventListener("submit", (event) => {
+    handleSubmit(event);
+  });
 
   function handleSubmit(event) {
-    event.preventDefault()
-    createNav.setAttribute('hidden', '')
+    event.preventDefault();
+    createNav.setAttribute("hidden", "");
   }
 }
 
@@ -204,20 +193,18 @@ function reminderForm() {
   reminderForm.appendChild(reminderBtn);
   reminderDiv.appendChild(reminderForm);
   createNav.appendChild(reminderDiv);
-  
-  reminderForm.addEventListener('submit', (event) => {
-    handleSubmit(event)
-  })
+
+  reminderForm.addEventListener("submit", (event) => {
+    handleSubmit(event);
+  });
 
   function handleSubmit(event) {
-    event.preventDefault()
-    createNav.setAttribute('hidden', '')
+    event.preventDefault();
+    createNav.setAttribute("hidden", "");
   }
 }
 
 //requests for API information and display
-
-
 
 //retrieve form data + add to database
 const userForm = document.getElementById("userForm");
@@ -273,5 +260,3 @@ async function fetchBookData(book) {
   bookCover.src = data.items[1].volumeInfo.imageLinks.smallThumbnail;
   bookContainer.appendChild(bookCover);
 }
-
-
